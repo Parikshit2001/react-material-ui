44749f36ddaf84a1700c08fb44bf864942415b48
parikshit <parikshitgupta2001@gmial.com>
2023-12-21 21:19:54 +0530

    Updated card component

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,11 +1,20 @@
 import { DeleteOutlined } from "@mui/icons-material"
-import { Card, CardContent, CardHeader, IconButton, Typography } from "@mui/material"
-
+import { Card, CardContent, CardHeader, CardMedia, Hidden, IconButton, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
 
 function CardComponent() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>setData(json));
+  }, [])
+
   return (
-    <div>
-      <Card elevation={3}>
+    <div style={{display:'flex', flexWrap:'wrap', gap:'16px'}}>
+      {/* <Card elevation={3}>
         <CardHeader
           action={
             <IconButton onClick={() => {console.log("Delete")}}>
@@ -20,7 +29,29 @@ function CardComponent() {
             ...Architecture is the artful fusion of form and function, shaping the physical environment we inhabit. It transcends mere construction, weaving together aesthetics, technology, and human experience. Each structure tells a unique story, reflecting the culture and aspirations of its time. In its harmonious blend of creativity and practicality, architecture stands as a testament to humanity&apos;s innate desire to craft spaces that inspire and endure.
           </Typography>
         </CardContent>
-      </Card>
+      </Card> */}
+      {data.map(item => (
+        <Card key={item.id} elevation={3} sx={{width:300, height:500}}>
+          <CardMedia
+            component="img"
+            height="194"
+            image={item.image}
+            alt="Paella dish"
+            sx={{height:250}}
+          />
+          <CardContent>
+            <Typography variant='h5' sx={{height: 50, overflow:'hidden', textOverflow:'ellipsis', whiteSpace:'nowrap'}}>
+              {item.title}
+            </Typography>
+            <Typography variant="body1" sx={{height:100, overflow:'hidden'}}>
+              {item.description}
+            </Typography>
+            <Typography variant="h1">
+              $ {item.price}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   )
 }
